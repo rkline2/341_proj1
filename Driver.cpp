ed9eb51f8e0f263d94af05672eb2990c9c54677b
Rooklyn <61075163+rkline2@users.noreply.github.com>
2020-06-11 10:39:05 -0400

    version 1.3

--- Driver.cpp
+++ Driver.cpp
@@ -22,11 +22,13 @@ using namespace std;
 int main() {
 	/*****change LN_SIZE here*****/
 	const int SIZE = 10;
-
 	int num_items = 0;
 	string usrResponce;
-
-	cout << "******Beginning Tests******" << endl;
+	cout << "******Beginning Tests******" << endl << endl;
+	cout << "You will be asked to enter any value once a test is completed" << endl;
+	cout << "Enter any value to continue: ";
+	cin >> usrResponce;
+	cout << endl;
 
 	// Test 1: Push function
 	cout << "Test 1: Push()" << endl;
@@ -54,7 +56,7 @@ int main() {
 	cout << "Copying data from stack_1 to new int stack_2" << endl;
 	Llama<int, SIZE>* stack_2 = new Llama<int, SIZE>(*stack_1);
 
-	cout << endl << "Displaying stack_1 & stack_2" << endl;
+	cout << endl << "------Displaying stack_1 & stack_2------" << endl;
 
 	cout << "******stack_1******" << endl;
 	stack_1->dump();
@@ -72,7 +74,7 @@ int main() {
 	for (int i = 0; i < num_items; i++) 
 		stack_1->pop();
 	
-	cout << endl << "Displaying stack_1 & stack_2" << endl;
+	cout << endl << "------Displaying stack_1 & stack_2------" << endl;
 	
 	cout << "******stack_1******" << endl;
 	stack_1->dump();
@@ -84,11 +86,11 @@ int main() {
 
 	// Test 4: Swap Function
 	cout << "Test 4: swap()" << endl;
-	cout << "Swapping stack_2" << endl;
-	cout << "Before swap:" << endl;
+	cout << "------Swapping stack_2------" << endl;
+	cout << "**Before swap:**" << endl;
 	stack_2->dump(); stack_2->swap();
 
-	cout << "After swap:" << endl;
+	cout << "**After swap:**" << endl;
 	stack_2->dump();
 
 	cout << "Enter any value to continue: ";
@@ -97,11 +99,11 @@ int main() {
 
 	// Test 5: Rotate Function
 	cout << "Test 5: rot()" << endl;
-	cout << "Rotating stack_1" << endl;
-	cout << "Before rotate:" << endl;
+	cout << "------Rotating stack_1------" << endl;
+	cout << "**Before rotate:**" << endl;
 	stack_1->dump(); stack_1->rot();
 
-	cout << "After rotate:" << endl;
+	cout << "**After rotate:**" << endl;
 	stack_1->dump();
 
 	cout << "Enter any value to continue: ";
@@ -115,6 +117,7 @@ int main() {
 	for (int i = 0; i < num_items; i++)
 		stack_2->dup();
 
+	cout << "------displaying stack_2------" << endl;
 	stack_2->dump();
 
 	cout << "Enter any value to continue: ";
@@ -123,7 +126,7 @@ int main() {
 
 	// Test 7: Assignment Operator
 	cout << "Test 7: Assignment Operator" << endl;
-	cout << "stack_2 = stack_1" << endl;
+	cout << "-----stack_2 = stack_1-----" << endl;
 	*stack_2 = *stack_1;
 
 	cout << "******stack_1******" << endl;
@@ -138,10 +141,10 @@ int main() {
 
 	// Test 8: Top Item && peek()
 	cout << "Test 8: Top && peek(0)" << endl;
-	cout << "stack_1 top: " << *stack_1->GetTop() << endl;
-	cout << "stack_1 peek(0): " << stack_1->peek(0) << endl;
+	cout << "stack_1 top: " << *stack_1->GetTop() << " memory: "<< stack_1->GetTop() << endl;
+	cout << "stack_1 peek(0): " << stack_1->peek(0) << stack_1->peek(0)<<endl;
 
-	cout << "stack_2 top: " << *stack_2->GetTop() << endl;
+	cout << "stack_2 top: " << *stack_2->GetTop() << " memory: " << stack_2->GetTop() << endl;
 	cout << "stack_2 peek(0): " << stack_2->peek(0) << endl;
 
 	cout << "Enter any value to continue: ";
